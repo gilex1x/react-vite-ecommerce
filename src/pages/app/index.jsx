202c1ba327555ffe96007e3d570a639fae9f8b92
root <root@DESKTOP-JGMP2PO>
2023-10-05 21:11:24 -0500

    []

--- src/pages/app/index.jsx
+++ src/pages/app/index.jsx
@@ -1,13 +1,31 @@
-import { useState } from 'react'
+import { useRoutes, BrowserRouter } from 'react-router-dom';
+import { useState } from 'react';
+import Home from '../home';
+import MyAccount from '../myAccount';
+import MyOrder from '../myOrder';
+import SingIn from '../singIn'
+import MyOrders from '../myOrders'
 import './App.css'
+
+
+const AppRouter = () => {
+  let routes = useRoutes([
+    {path: '/', element: <Home />},
+    {path: '/my-account', element: <MyAccount />},
+    {path: '/my-order', element: <MyOrder />},
+    {path: '/*', element: <MyOrder />},
+  ]);
+  return routes;
+}
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <BrowserRouter>
+        <AppRouter/>
+      </BrowserRouter>
+    </>
   )
 }
 
---
+++ src/pages/singIn/index.jsx
@@ -0,0 +1,9 @@
+const SingIn = () => {
+    return (
+        <>
+            <h1>Hello from singin</h1>
+        </>
+    )
+};
+
+export default SingIn
